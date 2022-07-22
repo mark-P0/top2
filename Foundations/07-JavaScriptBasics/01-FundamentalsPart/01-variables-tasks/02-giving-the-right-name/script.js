12d90936805c79b6ecefafa52448921643ff27d0
Mark P <mj.poso18@gmail.com>
2022-07-22 19:17:32 +0800

    Performed "Variables" exercise

---
+++ Foundations/07-JavaScriptBasics/01-FundamentalsPart/01-variables-tasks/02-giving-the-right-name/script.js
@@ -0,0 +1,9 @@
+/*  Giving the right name
+ *  importance: 3
+ *
+ *  1. Create a variable with the name of our planet. How would you name such a variable?
+ *  2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+ */
+
+let planetName = 'Earth';
+let currentVisitorName;
