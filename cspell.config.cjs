7e7091b8b8537bb3e025318646c9b4ba443b047b
Mark P <mj.poso18@gmail.com>
2023-02-08 20:27:11 +0800

    Move CSpell config into JS file

---
+++ cspell.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('@cspell/cspell-types').CSpellSettings} */
+const config = {
+  words: [
+    // 'backtick', // Exists but not "backticks"?
+    'backticks',
+    'templating',
+    'concat',
+    // 'whitespace',
+    'whitespaces',
+    'substr',
+    'Replit',
+    'IIFE',
+    'pseudocode',
+    'pseudocoding',
+    'subrepo',
+    'subrepos',
+    'instanceof',
+    'BSON',
+    'bulma',
+    'preprocessors',
+    'precompilers',
+  ],
+  allowCompoundWords: true,
+};
+
+module.exports = config;
